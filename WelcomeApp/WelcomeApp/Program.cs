2e421d6d00026a79fc6750ab825a85894bef7fc1
brady smith <bradysmith1019@gmail.com>
2020-07-07 23:53:54 -0600

    Added database functionality

--- WelcomeApp/WelcomeApp/Program.cs
+++ WelcomeApp/WelcomeApp/Program.cs
@@ -11,6 +11,8 @@ using Microsoft.Win32;
 using Newtonsoft.Json;
 using HtmlAgilityPack;
 using System.Net.Mail;
+using MySql.Data;
+using MySql.Data.MySqlClient;
 
 /* Console application that displays various weather information obtained 
  * via the Dark Sky API and the flavor of the day for my local Culver's.
@@ -20,7 +22,7 @@ namespace WelcomeApp
     class Program
     {
         // Placeholder for the required API key for accessing the Dark Sky API
-        private const string API_KEY = "";
+        private const string API_KEY = "74daa3f39f330feb4ead7e52c3cb4352";
 
         // URL used to obtain the forecast. Latitude and Longitude are determined using a GeoCoordinateWatcher
         private const string ForecastURL = "https://api.darksky.net/forecast/" + API_KEY + "/@LAT@,@LON@?exclude=alerts,flags,minutely?units=auto";
@@ -29,7 +31,18 @@ namespace WelcomeApp
         static GeoCoordinateWatcher watcher = new GeoCoordinateWatcher();
 
         // Holds the current flavor of the day
-        public static string flavor = "";        
+        public static string flavor = "";
+
+        public static string high = "";
+
+        public static string low = "";
+
+        public static string temp = "";
+
+        public static string description = "";
+
+        public static string connectionString = "server=localhost;database=welcomeapp;uid=root;password=BSmith47";
+
 
         /* Entry point for the application
          */
@@ -64,11 +77,19 @@ namespace WelcomeApp
             // the flavor of the day
             sendText();
 
+            
+
             Console.Write("  Retrieving Location... ");
 
             // Retrieves the current location
-            watcher.StatusChanged += WatcherStatusChanged;
-            watcher.Start();
+            //watcher.StatusChanged += WatcherStatusChanged;
+            watcher.TryStart(true, TimeSpan.FromMilliseconds(1000));
+            GeoCoordinate coord = watcher.Position.Location;
+            WatcherStatusChanged(coord.Latitude, coord.Longitude);
+
+            AddToDB();
+
+            ShowOptional();
 
             // Calls the method that makes this program run on computer startup
             SetStartup();
@@ -78,19 +99,19 @@ namespace WelcomeApp
          * Gets added to the watcher object in the event that the
          * watcher's status has changed
          */
-        private static void WatcherStatusChanged(object sender, GeoPositionStatusChangedEventArgs e)
+        private static void WatcherStatusChanged(double lat, double lon)
         {
-            if (e.Status == GeoPositionStatus.Ready)
-            {
+            //if (e.Status == GeoPositionStatus.Ready)
+            //{
                 Console.Write("Location Found\n\n");
 
                 // Used to hold the coordinates of the current location
-                GeoCoordinate coord = watcher.Position.Location;
+                //GeoCoordinate coord = watcher.Position.Location;
 
                 // Replaces the latitude and longitude in the forecast URL so the proper URL
                 // can be accessed
-                string forUrl = ForecastURL.Replace("@LAT@", coord.Latitude.ToString());
-                forUrl = forUrl.Replace("@LON@", coord.Longitude.ToString());
+                string forUrl = ForecastURL.Replace("@LAT@", lat.ToString());
+                forUrl = forUrl.Replace("@LON@", lon.ToString());
 
                 using (WebClient client = new WebClient())
                 {
@@ -100,13 +121,13 @@ namespace WelcomeApp
                         DisplayCurrentTemp(client.DownloadString(forUrl));
                     }
 
-                    catch (Exception)
+                    catch (Exception e)
                     {
-                        Console.WriteLine("Error retrieving current temperature");
+                        Console.WriteLine(e);
                     }
                    
                 }
-            }
+            //}
         }
 
         /* Used to retrieve the current flavor of the day from
@@ -127,6 +148,7 @@ namespace WelcomeApp
 
             if (node != null)
             {
+
                 foreach (HtmlNode link in node)
                 {
                     if (theOne)
@@ -147,7 +169,7 @@ namespace WelcomeApp
 
                     // It was found that the line after the line that reads "View Next Month"
                     // is the line with the flavor of the day
-                    if (link.InnerText.Equals("View Next Month"))
+                    if (link.InnerText.Equals("View Next Month") || link.InnerText.Equals("View Entire Month"))
                     {
                         theOne = true;
                     }
@@ -199,7 +221,7 @@ namespace WelcomeApp
                 string carrier_email = "@txt.att.net";
 
                 // Recipient's phone number goes here as a string
-                string phone = "";
+                string phone = "9202856365";
 
                 // Constructs an email using the recipient's phone number and service provider
                 string to_email = phone + carrier_email;
@@ -207,10 +229,10 @@ namespace WelcomeApp
                 // Calls the send email method with the recipient's name, the sender's email address, and the sender's
                 // email address password in the blank strings. Sends the email, which sends the text to the recipient
                 // with the flavor of the day
-                SendEmail("", to_email,
-                    "Culvers", "",
+                SendEmail("Ashley", to_email,
+                    "Culvers", "bradyboy95@hotmail.com",
                     "smtp.live.com", 587,
-                    true, "",
+                    true, "BSmith47",
                     "Flavor of the Day", "The flavor of the day is " + flavor);
             }
             catch (Exception ex)
@@ -235,8 +257,11 @@ namespace WelcomeApp
             // Holds the degree symbol to be used in displaying the temperature
             char degree = (char)176;
 
-            Console.WriteLine("  Summary: " + currently["summary"] + "\n");
-            Console.WriteLine("  Current temperature is " + currently["temperature"] + degree.ToString() + "\n");
+            temp = currently["temperature"] + degree.ToString();
+            description = currently["summary"];
+
+            Console.WriteLine("  Summary: " + description + "\n");
+            Console.WriteLine("  Current temperature is " + temp + "\n");
 
             // Gets the information regarding the hourly conditions
             dynamic hourly = response["hourly"];
@@ -246,9 +271,10 @@ namespace WelcomeApp
 
             foreach (dynamic d in daily["data"])
             {
-
-                Console.WriteLine("  High for today:   " + d["temperatureHigh"] + degree.ToString() + "\n");
-                Console.WriteLine("  Low for today:   " + d["temperatureLow"] + degree.ToString() + "\n");
+                high = d["temperatureHigh"] + degree.ToString();
+                low = d["temperatureLow"] + degree.ToString();
+                Console.WriteLine("  High for today:   " + high + "\n");
+                Console.WriteLine("  Low for today:   " + low + "\n");
 
                 // Converts the sunset time into a readable format
                 DateTimeOffset sunsetTime = DateTimeOffset.FromUnixTimeSeconds((long)d["sunsetTime"]);
@@ -327,6 +353,105 @@ namespace WelcomeApp
                 }
 
                 Console.WriteLine("  " + noColon[0] + " " + split[2] + ":\t     " + data["temperature"] + degree.ToString() + "\t" + data["summary"] + "\n");
+            }
+        }
+
+        private static void AddToDB()
+        {
+            if (temp.Equals("") || high.Equals("") || low.Equals("") || description.Equals("") || flavor.Equals(""))
+            {
+                return;
+            }
+
+            using (MySqlConnection conn = new MySqlConnection(connectionString))
+            {
+                try
+                {
+                    conn.Open();
+
+                    MySqlCommand command = conn.CreateCommand();
+                    command.CommandText = "insert ignore into weather (Date, Temperature, High, Low, Description) values (@date, @temp, @high, @low, @description); insert ignore into flavors (Date, Flavor) values (@date, @flavor);";
+                    command.Parameters.AddWithValue("@date", DateTime.Now.ToString("yyyy.MM.dd"));
+                    command.Parameters.AddWithValue("@temp", temp);
+                    command.Parameters.AddWithValue("@high", high);
+                    command.Parameters.AddWithValue("@low", low);
+                    command.Parameters.AddWithValue("@description", description);
+                    command.Parameters.AddWithValue("@flavor", flavor);
+
+                    command.ExecuteNonQuery();
+                }
+
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+        }
+
+        private static void ShowOptional()
+        {
+            string next;
+            while ((next = Console.ReadLine()) != null)
+            {
+                if (next == "show weather")
+                {
+                    using (MySqlConnection conn = new MySqlConnection(connectionString))
+                    {
+                        try
+                        {
+                            conn.Open();
+
+                            MySqlCommand command = conn.CreateCommand();
+                            command.CommandText = "select * from weather;";
+
+                            MySqlDataReader myReader;
+                            myReader = command.ExecuteReader();
+                            Console.WriteLine("\r\n   Date     | Temperature |  High  |   Low   |  Description");
+                            Console.WriteLine("------------+-------------+--------+---------+-----------------");
+                            try
+                            {
+                                while (myReader.Read())
+                                {
+                                    if (myReader["Date"].ToString().Split(' ')[0].Length == 8)
+                                    {
+                                        if (myReader["Low"].ToString().Length == 3)
+                                        {
+                                            Console.WriteLine(myReader["Date"].ToString().Split(' ')[0] + "    |    " + myReader["Temperature"] + "     |  " + myReader["High"] + "  |   " + myReader["Low"] + "   |  " + myReader["Description"]);
+
+                                        }
+                                        else
+                                        {
+                                            Console.WriteLine(myReader["Date"].ToString().Split(' ')[0] + "    |    " + myReader["Temperature"] + "     |  " + myReader["High"] + "  |   " + myReader["Low"] + "  |  " + myReader["Description"]);
+
+                                        }
+                                    }
+                                    else
+                                    {
+                                        if (myReader["Low"].ToString().Length == 3)
+                                        {
+                                            Console.WriteLine(myReader["Date"].ToString().Split(' ')[0] + "   |    " + myReader["Temperature"] + "     |  " + myReader["High"] + "  |   " + myReader["Low"] + "   |  " + myReader["Description"]);
+
+                                        }
+                                        else
+                                        {
+                                            Console.WriteLine(myReader["Date"].ToString().Split(' ')[0] + "   |    " + myReader["Temperature"] + "     |  " + myReader["High"] + "  |   " + myReader["Low"] + "  |  " + myReader["Description"]);
+                                        }
+                                    }
+                                }
+                            }
+                            finally
+                            {
+                                myReader.Close();
+                            }
+                        }
+
+                        catch (Exception e)
+                        {
+                            Console.WriteLine(e.Message);
+                        }
+                    }
+                }
+
             }
         }
 
